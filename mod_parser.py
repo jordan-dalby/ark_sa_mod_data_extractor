f803d378f3c2e6c6e0f678d65de5ccb08cb6b24a
Jordan Dalby <jordan.dalby@outlook.com>
2024-04-09 18:43:54 +0100

    Re-factored mod_parser.py, moved common functionality to a Utils class

--- mod_parser.py
+++ mod_parser.py
@@ -13,36 +13,58 @@ import unreal
 BEACON_NAMESPACE = uuid.UUID("82aa4465-85f9-4b9e-8d36-f66164cef0a6")
 
 
-class UnrealParser:
-    """Parser used for getting data from the engine directly"""
+class MissingDataException(Exception):
+    """Raised when crucial data is missing"""
 
-    def __init__(self, mod_root_folder):
-        self.mod_root_folder = mod_root_folder
-
-    def find_mda(self):
-        """Find the mod data asset for this mod"""
-        # pylint: disable=no-member
-        assets = unreal.EditorAssetLibrary.list_assets(self.mod_root_folder, recursive=True)
-        for asset_path in assets:
-            asset_name = unreal.Paths.get_base_filename(asset_path)
-            if asset_name.startswith("ModDataAsset"):
-                return asset_path
-        return None
-
-    def get_additional_engram_blueprint_classes(self, asset_path):
-        """Gets the additional engram blueprint classes field from the mod data asset"""
-        # pylint: disable=no-member
-        asset_object = unreal.EditorAssetLibrary.load_asset(asset_path)
-        if asset_object:
-            additional_engram_blueprint_classes = asset_object.additional_engram_blueprint_classes
-            if additional_engram_blueprint_classes:
-                return additional_engram_blueprint_classes
-        else:
-            print("Failed to load asset:", asset_path)
-        return None
+    def __init__(self, message):
+        super().__init__(message)
 
 
-class BeaconBuilder:
+class MissingArgumentException(Exception):
+    """Raised when crucial arguments is missing"""
+
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class Utils:
+    """A class with some helpful utilities"""
+
+    @classmethod
+    def make_tmp_dir(cls):
+        """Creates and returns a temporary directory"""
+        tmp_dir = tempfile.mkdtemp()
+        return tmp_dir
+
+    @classmethod
+    def remove_tmp_dir(cls, tmp_dir):
+        """Removes a previously created temporary directory"""
+        shutil.rmtree(tmp_dir)
+
+    @classmethod
+    def dump_to_file(cls, tmp_dir, file_name, contents):
+        """Dumps data to a file given a directory, a file name, and its contents"""
+        with open(os.path.join(tmp_dir, file_name), "w", encoding="utf-8") as text_file:
+            text_file.write(contents)
+
+    @classmethod
+    def is_valid_directory(cls, path):
+        """Checks that a given directory is valid"""
+        if not os.path.isdir(path):
+            raise argparse.ArgumentTypeError(f"{path} is not a valid directory")
+        return path
+
+    @classmethod
+    def create_beacondata(cls, folder_path, output_path):
+        """Create the beacondata file"""
+        with tarfile.open(output_path, "w:gz") as tar:
+            for root, _, files in os.walk(folder_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    tar.add(file_path, arcname=os.path.relpath(file_path, folder_path))
+
+
+class BeaconEngramBuilder:
     """A builder class that creates files in a format that can be imported into Beacon"""
 
     def __init__(self, mod_parser):
@@ -52,7 +74,7 @@ class BeaconBuilder:
 
     def add_engram(self, engram_data):
         """Adds an engram to the engrams array"""
-        blueprintable = "blueprintable" if engram_data["blueprintable"] else None
+        blueprintable = "blueprintable" if engram_data["blueprintable"] is True else None
         engram = {
             "group": "engrams",
             "engramId": engram_data["uuid"],
@@ -84,14 +106,6 @@ class BeaconBuilder:
 
         self.engrams.append(engram)
 
-    def create_beacondata(self, folder_path, output_path):
-        """Create the beacondata file"""
-        with tarfile.open(output_path, "w:gz") as tar:
-            for root, _, files in os.walk(folder_path):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    tar.add(file_path, arcname=os.path.relpath(file_path, folder_path))
-
     def build(self, tmp_dir):
         """Build the contents of the json file that Beacon uses"""
         content_pack = [
@@ -109,7 +123,7 @@ class BeaconBuilder:
         ]
         content_packs = {"gameId": "ArkSA", "contentPacks": content_pack}
         content = {"payloads": [content_packs, {"gameId": "ArkSA", "engrams": self.engrams}]}
-        self.mod_parser.dump_to_file(
+        Utils.dump_to_file(
             tmp_dir,
             f"{self.mod_parser.mod_data['content_pack_id']}.json",
             json.dumps(content, indent=4),
@@ -125,20 +139,20 @@ class BeaconBuilder:
             "files": [f"{self.mod_parser.mod_data['content_pack_id']}.json"],
             "isUserData": True,
         }
-        self.mod_parser.dump_to_file(tmp_dir, "Manifest.json", json.dumps(manifest, indent=4))
+        Utils.dump_to_file(tmp_dir, "Manifest.json", json.dumps(manifest, indent=4))
 
     def dump(self):
         """Dump all of the data to the temp directly and create the beacondata file"""
-        temp_dir = self.mod_parser.make_tmp_dir()
+        temp_dir = Utils.make_tmp_dir()
         self.build(temp_dir)
         self.build_manifest(temp_dir)
         file_name = f"{self.mod_parser.mod_data['mod_name']}.beacondata"
         self.output_file_path = os.path.join(self.mod_parser.output_dir, file_name)
-        self.create_beacondata(temp_dir, self.output_file_path)
-        self.mod_parser.remove_tmp_dir(temp_dir)
+        Utils.create_beacondata(temp_dir, self.output_file_path)
+        Utils.remove_tmp_dir(temp_dir)
 
 
-class StandardBuilder:
+class StandardEngramBuilder:
     """A standard builder that returns generic json extracted from the mod"""
 
     def __init__(self, mod_parser):
@@ -179,7 +193,7 @@ class StandardBuilder:
         """Dump the json data"""
         data = self.build()
         file_name = f"{self.mod_parser.mod_data['mod_name']}-data.json"
-        self.mod_parser.dump_to_file(
+        Utils.dump_to_file(
             self.mod_parser.output_dir,
             file_name,
             data,
@@ -187,7 +201,7 @@ class StandardBuilder:
         self.output_file_path = os.path.join(self.mod_parser.output_dir, file_name)
 
 
-class Builder:
+class EngramBuilder:
     """Base builder class, allows user to choose which output they prefer"""
 
     def __init__(self, mod_parser, builder):
@@ -247,65 +261,34 @@ class ModParser:
                 return str(uuid.uuid5(BEACON_NAMESPACE, f"{content_pack_id.lower()}:{path.lower()}"))
         return None
 
-    def make_tmp_dir(self):
-        """Creates and returns a temporary directory"""
-        tmp_dir = tempfile.mkdtemp()
-        return tmp_dir
-
-    def remove_tmp_dir(self, tmp_dir):
-        """Removes a previously created temporary directory"""
-        shutil.rmtree(tmp_dir)
-
-    def dump_to_file(self, tmp_dir, file_name, contents):
-        """Dumps data to a file given a directory, a file name, and its contents"""
-        with open(os.path.join(tmp_dir, file_name), "w", encoding="utf-8") as text_file:
-            text_file.write(contents)
-
-    def is_valid_directory(self, path):
-        """Checks that a given directory is valid"""
-        if not os.path.isdir(path):
-            raise argparse.ArgumentTypeError(f"{path} is not a valid directory")
-        return path
-
     def parse_arguments(self):
         """Parses arguments from CLI"""
         parser = argparse.ArgumentParser(description="Description of your program")
 
         subparsers = parser.add_subparsers(dest="subcommand", title="Subcommands", description="Choose a subcommand")
 
+        # Beacon subparser, requires more inputs than the standard parser does
         parser_beacon = subparsers.add_parser("beacon", help="Use the Beacon parser to generate a .beacondata file")
         parser_beacon.add_argument("--mod-root-folder", type=str, help="Root folder of the mod")
         parser_beacon.add_argument("--mod-id", type=str, help="ID of the mod")
         parser_beacon.add_argument("--mod-name", type=str, help="Name of the mod")
         parser_beacon.add_argument("--content-pack-id", type=str, help="ID of the content pack")
-        parser_beacon.add_argument("--output-folder", type=self.is_valid_directory, help="Output folder")
+        parser_beacon.add_argument("--output-folder", type=Utils.is_valid_directory, help="Output folder")
 
+        # Standard parser
         parser_standard = subparsers.add_parser("standard", help="Activate standard")
         parser_standard.add_argument("--mod-root-folder", type=str, help="Root folder of the mod")
         parser_standard.add_argument("--output-folder", type=str, help="Output folder")
         parser_standard.add_argument("--mod-name", type=str, help="Name of the mod")
 
+        # Parse the args and ensure we got all the ones we need
         args = parser.parse_args()
+        missing_args = [arg_name for arg_name, arg_value in vars(args).items() if arg_value is None]
+        if missing_args:
+            raise MissingArgumentException(f"Not all arguments were provided, missing: {missing_args}")
+
+        # Distribute the args to their relevant places
         subcommand = args.subcommand
-
-        if subcommand == "beacon":
-            if not all(
-                [
-                    args.mod_root_folder,
-                    args.mod_id,
-                    args.mod_name,
-                    args.content_pack_id,
-                    args.output_folder,
-                ]
-            ):
-                parser_beacon.error("All arguments are required for 'beacon' subcommand.")
-        elif subcommand == "standard":
-            if not all([args.mod_root_folder, args.output_folder, args.mod_name]):
-                parser_standard.error(
-                    """Arguments 'output_folder' and 'mod_name' are required for 
-                    'standard' subcommand."""
-                )
-
         if subcommand == "beacon":
             self.mod_data["content_pack_id"] = args.content_pack_id
             self.mod_data["mod_id"] = args.mod_id
@@ -318,28 +301,48 @@ class ModParser:
 
         return args
 
+    def find_mda(self):
+        """Find the mod data asset for this mod"""
+        # pylint: disable=no-member
+        assets = unreal.EditorAssetLibrary.list_assets(self.mod_data["mod_root_folder"], recursive=True)
+        for asset_path in assets:
+            asset_name = unreal.Paths.get_base_filename(asset_path)
+            # Since we ask for the Mod Path in the command, we can be sure this is the correct MDA
+            if asset_name.startswith("ModDataAsset"):
+                return asset_path
+        raise MissingDataException("Could not find a ModDataAsset file in the given mod_root_folder")
+
+    def get_additional_engram_blueprint_classes(self, asset_path):
+        """Gets the additional engram blueprint classes field from the mod data asset"""
+        # pylint: disable=no-member
+        asset_object = unreal.EditorAssetLibrary.load_asset(asset_path)
+        if asset_object:
+            additional_engram_blueprint_classes = asset_object.additional_engram_blueprint_classes
+            if additional_engram_blueprint_classes:
+                return additional_engram_blueprint_classes
+        else:
+            raise MissingDataException(f"Failed to load asset: {asset_path}")
+        return None
+
     def run(self):
         """Run the parser"""
-        unreal_parser = UnrealParser(self.mod_data["mod_root_folder"])
-        mda_asset_path = unreal_parser.find_mda()
-        if mda_asset_path:
-            print("Found ModDataAsset_BP asset:", mda_asset_path)
-            builder = Builder(
-                self,
-                (BeaconBuilder(mod_parser=self) if self.parser == "beacon" else StandardBuilder(mod_parser=self)),
-            )
-            engram_entries = unreal_parser.get_additional_engram_blueprint_classes(mda_asset_path)
-            for engram in engram_entries:
-                # pylint: disable=no-member
-                engram_obj = unreal.load_object(None, engram.get_path_name())
-                # pylint: disable=no-member
-                engram_obj_default = unreal.get_default_object(engram_obj)
-
-                builder.add_engram(engram_obj_default)
-            builder.dump()
-            print(f"Done! {builder.get_output_file()}")
-        else:
-            print("ModDataAsset_BP asset not found.")
+        print("Starting ARK SA Mod Data Parser")
+        mda_asset_path = self.find_mda()
+        print(f"Found ModDataAsset_BP asset: {mda_asset_path}")
+        builder = EngramBuilder(
+            self,
+            (BeaconEngramBuilder(mod_parser=self) if self.parser == "beacon" else StandardEngramBuilder(mod_parser=self)),
+        )
+        engram_entries = self.get_additional_engram_blueprint_classes(mda_asset_path)
+        print(f"{len(engram_entries)} engram entries found.")
+        for engram in engram_entries:
+            # pylint: disable=no-member
+            engram_obj = unreal.load_object(None, engram.get_path_name())
+            # pylint: disable=no-member
+            engram_obj_default = unreal.get_default_object(engram_obj)
+            builder.add_engram(engram_obj_default)
+        builder.dump()
+        print(f"Done! {builder.get_output_file()}")
 
 
 ModParser().run()
